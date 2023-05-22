e61a01677813e5c90ceadfb33c35f9b3d41a0e4c
julian23alvarado <julian.d.alvarado23@gmail.com>
2023-05-21 23:02:33 -0500

    base project refactor

---
+++ src/business/entities/QueryObject.java
@@ -0,0 +1,20 @@
+package business.entities;
+
+public class QueryObject {
+
+    private String column1;
+    private String column2;
+    private String column3;
+    private String column4;
+    private String column5;
+    private String column6;
+    private String column7;
+    private String column8;
+    private String column9;
+    private String column10;
+
+
+
+
+
+}
--- src/controllers/IndexController.java
+++ src/controllers/IndexController.java
@@ -2,24 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
  */
-package controladores;
+package controllers;
 
-import static java.awt.SystemColor.window;
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
+
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.MenuItem;
-import javafx.stage.Stage;
 
 /**
  * FXML Controller class
